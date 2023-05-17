40358b9aa104dbced6c471eb85f78ee2727cc6da
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-05-17 13:43:14 -0300

    :construction:

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,7 +5,7 @@ import hexToRgba from 'hex-to-rgba';
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
-            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.cor} type='color' className='input-cor' />
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-cor' />
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => { 
